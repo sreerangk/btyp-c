ac3d103e185bb52cd8dfdf16453f34797d95f147
sreerangk <sreerangk77@gmail.com>
2022-07-22 20:53:34 +0530

    16

--- prime.java
+++ prime.java
@@ -1,16 +1,27 @@
-import java.util.Scanner;
-public class prime{
-  public static void main(String ar[]) {
-    Scanner sc=new Scanner(System.in);
-    System.out.println("enter the number \n");
-    int a=sc.nextInt();
-    for(int i=2;i<a/2;i++){
-        if(a%i==0){
-            System.out.println("numbr",a);
-            
-        }
-    }
-    
-    System.out.println("");
-  }
-}
+import java.util.Scanner;   
+  
+public class prime {  
+  
+   public static void main(String[] args) {  
+       Scanner s = new Scanner(System.in);  
+       System.out.print("Enter a number : ");  
+       int n = s.nextInt();  
+       if (isPrime(n)) {  
+           System.out.println(n + " is a prime number");  
+       } else {  
+           System.out.println(n + " is not a prime number");  
+       }  
+   }  
+  
+   public static boolean isPrime(int n) {  
+       if (n <= 1) {  
+           return false;  
+       }  
+       for (int i = 2; i < Math.sqrt(n); i++) {  
+           if (n % i == 0) {  
+               return false;  
+           }  
+       }  
+       return true;  
+   }  
+}  
