379f284140714d8b54d360aafc8c6abfff8e39d0
sreerangk <sreerangk77@gmail.com>
2022-07-22 15:17:23 +0530

    15

--- add2dary-14.c
+++ add2dary-14.c
@@ -8,15 +8,29 @@ int main(){
         for(j=1;j<=limit;j++){
             scanf("%d",&a[i][j]);
         }
-       printf("\n");
     }
     printf("enter the value of array 2:");
     for(i=1;i<=limit;i++){
         for(j=1;j<=limit;j++){
             scanf("%d",&b[i][j]);
         }
+    }
+    printf("arrya-1\n");
+    for(i=1;i<=limit;i++){
+        for(j=1;j<=limit;j++){
+            printf("%d ",a[i][j]);
+        }
         printf("\n");
     }
+    printf("arrya-2\n");
+    for(i=1;i<=limit;i++){
+        for(j=1;j<=limit;j++){
+            printf("%d ",b[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Sum of 2 arrays is:\n");
     for(i=1;i<=limit;i++){
         for(j=1;j<=limit;j++){
             c[i][j]=a[i][j]+b[i][j];
