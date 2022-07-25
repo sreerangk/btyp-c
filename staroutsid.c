917d61e47d745977acb54626387bcd59e1f799aa
sreerangk <sreerangk77@gmail.com>
2022-07-25 22:26:47 +0530

    26

--- staroutsid.c
+++ staroutsid.c
@@ -10,14 +10,30 @@
 // *********
 #include<stdio.h>
 int main(){
-    int i,j,k,n,sum=0;
-    printf("enter the number ");
-    scanf("%d",&n);
-    for(i=1;i<n;i++){
-     sum=i+i;
-     k=sum+i;
-
-     printf("%d",k);
+    int i,j,k,l,h;
+    printf("enter the limit");
+    scanf("%d",&h);
+    for(i=1;i<h;i++){
+        if(i==1){
+        for(j=1;j<=2;j++){
+            for(l=1;l<=i;l++){
+                printf("*");
+            }
+            printf("\n");
+        }
+        }
+        else{
+        for(j=1;j<=i;j++){
+          for(l=1;l<=i;l++){
+        
+            printf("*");
+        }
+        printf("\n");
+        }
+        }
+        for(k=1;k<=i*3;k++){
+            printf("*");
+        }
+     printf("\n");
        
-}
-}
+}}
