59a1a2f38b50ca8d221be7fca1764422c8ed1f3b
sreerangk <sreerangk77@gmail.com>
2022-07-25 23:14:50 +0530

    25-2

---
+++ str25-2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i,j,limit;
+    printf("enter the number ");
+    scanf("%d",&limit);
+    for(i=1;i<=limit;i++){
+        for(j=1;j<=i*2;j++){
+            printf("*");
+        }
+        printf("\n");
+        for(j=1;j<=i*3;j++){
+            printf("*\n");
+        }
+    }
+}
