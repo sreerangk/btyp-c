b209019e8d5bcf175fdde09f26d438eab0b140af
sreerangk <sreerangk77@gmail.com>
2022-07-26 09:31:02 +0530

    29

---
+++ loop25-2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int i,j,limit,k;
+    printf("enter the number :");
+    scanf("%d",&limit);
+    for(i=1;i<=limit;i++){
+        for(j=1;j<=i*2;j++){
+            printf("*\n");
+        }
+
+    for(j=1;j<=i*3;j++){
+        printf("*");
+    }
+    printf("\n");
+    }
+    }
