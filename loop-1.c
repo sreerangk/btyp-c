da8aa09d99c63e602c9cb9dc844b2c471d6b692a
sreerangk <sreerangk77@gmail.com>
2022-07-27 12:34:03 +0530

    LOOP END

--- loop-1.c
+++ loop-1.c
@@ -4,12 +4,13 @@ int main(){
     printf("enter the limit");
     scanf("%d",&n);
     for(i=1;i<=n;i++){
-        for(j=1;j<=i*2;j++){
+        for(j=1;j<=i*3;j++){
             printf("*");
         }
-        printf("\n");
-        for(j=1;j<=i*3;j++){
+         printf("\n");
+        for(j=1;j<=i;j++){
             printf("*\n");
         }
+       
     }
 }
