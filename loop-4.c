7c8fa2aba3a51dc28713e149a3acfe360a90851c
sreerangk <sreerangk77@gmail.com>
2022-08-01 09:50:34 +0530

    chng

--- loop-4.c
+++ loop-4.c
@@ -4,7 +4,7 @@ int main(){
     printf("enter the number");
     scanf("%d",&n);
     for(i=1;i<=n;i++){
-        for(j=1;j<=i*1;j++){
+        for(j=1;j<=i;j++){
             printf("*\n");
         }
     
