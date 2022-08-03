55f6854588500f9bf242b296dafeddc4662a0136
sreerangk <sreerangk77@gmail.com>
2022-08-03 21:19:30 +0530

    s

--- pttn.c
+++ pttn.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 int main(){
     int i,j,n;
-    for(i=0;i<=5;i++){
-        for(j=5;j>=i;j--){
-            printf("*");
-        }
-        for(j=i;j>=0;j--){
+    for(i=1;i<=5;i++){
+        for(j=1;j<=i;j++){
             printf("%d",j);
         }
-   
-        for(j=1;j<=i;j++){
+        
+        for(j=i-1;j>=1;j--){
+          
             
             printf("%d",j);
         }
